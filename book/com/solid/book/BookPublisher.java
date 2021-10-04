5344995d29cb576b72b4b83c3cd3c57fdd44fa8c
Nitipat <62010494@kmitl.ac.th>
2021-10-04 22:28:16 +0700

    Fix

--- book/com/solid/book/BookPublisher.java
+++ book/com/solid/book/BookPublisher.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class BookPublisher {
     public static void main(String[] args) {
-        Book book = new PrintToFile("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        PrintToFile book = new PrintToFile("Tyland", List.of("I", "moved", "here", "recently", "too"));
         book.printToFile();
     }
 }
--- book/com/solid/book/BookReader.java
+++ book/com/solid/book/BookReader.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class BookReader {
     public static void main(String[] args) {
-        Book book = new PrintToScreen("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        PrintToScreen book = new PrintToScreen("Tyland", List.of("I", "moved", "here", "recently", "too"));
         book.printToScreen();
     }
 }
