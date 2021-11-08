c9e97a19ff4154c98314edc97db33719d37a5255
Nitipat <62010494@kmitl.ac.th>
2021-11-08 18:32:25 +0700

    Lambda

--- Behavior Design Patterns 2/src/behavior/HeightSortStrategy.java
+++ Behavior Design Patterns 2/src/behavior/HeightSortStrategy.java
@@ -7,12 +7,12 @@ public class HeightSortStrategy implements SortStrategy {
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
     @Override
     public void customSort(List<Person> people) {
-        // people.sort((a,b) -> a.getHeight().compareTo(b.getHeight())); // Lambda Expression
-        people.sort(new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                return o1.getHeight().compareTo(o2.getHeight());
-            }
-        });
+        people.sort((a,b) -> a.getHeight().compareTo(b.getHeight())); // Lambda Expression
+        // people.sort(new Comparator<Person>() {
+        //     @Override
+        //     public int compare(Person o1, Person o2) {
+        //         return o1.getHeight().compareTo(o2.getHeight());
+        //     }
+        // });
     }
 }
